28e516176cf3d5432ca59f31f63b49fd5418c244
JeanFrancoKuz <151879794+JeanFrancoKuz@users.noreply.github.com>
2025-06-23 20:22:53 -0400

    Create variables.py

---
+++ Python/Practicas/variables.py
@@ -0,0 +1,69 @@
+#Para hacer comentarios es de esta forma funciona paraa una sola linea
+"""
+  Hola, 
+  Yo soy un comentario para 
+  mas de una linea
+"""
+
+#Variables
+"""
+Es una forma de almacenar un valor en memoria
+"""
+name1 = "Jean"
+
+#Tipos de datos
+
+#Numeros enteros (Datos enteros)
+
+age1 = 23
+
+#Numeros flotantes (Datos con decimales)
+
+estatura = 1.75
+
+
+#Numeros Enteros negativos (Datos enteros negativos)
+
+dinero = -75
+
+#Numeros flotantes negativos (Datos enteros negativos)
+
+temperatura = -1.75
+
+
+#Cadena de caracteres (STRINGS)
+
+lasname1="zanahoria"
+
+#BooLeanos (Verdaderi o falso)
+isStudent = True
+
+isEmployed = False
+
+#Listas(Colecciones de elementos)
+
+fruits = ["apple","banana","cherry"]
+
+number = [1,2,3,4,5]
+
+students = [ "jean","fabiola","jeanet" ]
+
+components =["CPU","RAM","GPU",["SSD","HDD"]]
+
+#Tuplas(Colecciones de elemntos inmutables)
+fruits = ("apple","banana","cherry")
+
+number = (1, 2, 3, 4, 5)
+
+#Diccionarios (Colecciones de pared clave-valor)
+student ={
+  "name": "jean",
+  "age":23,
+  "isStudent":True
+}
+mensaje="holamundo"
+
+name, lastname, age="jean","kuzniar",23
+
+#pintar en consola
+print (name,lastname,age,mensaje)
